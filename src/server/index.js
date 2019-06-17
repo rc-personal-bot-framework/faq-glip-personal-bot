a2231f9fdcdfa744efd69afda22fcebdbc3cd25d
ZHAO Xudong <zxdong@gmail.com>
2019-06-17 16:16:37 +0800

    init

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+import * as skillFaq from 'ringcentral-personal-chatbot-skill-faq'
+import skillPack from 'ringcentral-personal-bot-skill-pack-simple'
+
+export const name = 'Demo Bot'
+export const description = 'Personal Chatbot with skills'
+export const homepage = 'https://github.com/rc-personal-bot-framework/ringcentral-personal-bot-template-js#readme'
+export const skills = [skillFaq, ...skillPack]
+
+/*
+export const onPostAdd = async ({
+  text, // original text
+  textFiltered, // text without metion user
+  group,
+  user,
+  handled, // hanlded by prev skills
+  shouldUseSignature // should use signature like "send by bot skill xxx" in message.
+}) => {
+  if (handled) {
+    return false
+  }
+  let sign = shouldUseSignature
+    ? `\n(send by [${exports.name}](${exports.homepage}))`
+    : ''
+  console.log(text, textFiltered, group, user, handled, shouldUseSignature)
+  if (textFiltered.toLowerCase() === 'hello') {
+    await user.sendMessage(group.id, {
+      text: 'Hi' + sign
+    })
+  }
+}
+*/
