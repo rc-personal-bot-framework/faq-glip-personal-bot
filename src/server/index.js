91ead966e56c2865012cac86f0dc38b589a6660e
ZHAO Xudong <zxdong@gmail.com>
2019-08-12 14:46:25 +0800

    Add view-user api

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
 import * as skillFaq from 'ringcentral-personal-chatbot-skill-faq'
 import * as skillTime from 'glip-personal-bot-skill-time'
 // import skillPack from 'ringcentral-personal-bot-skill-pack-simple'
+import basicAuth from 'express-basic-auth'
+import { User } from 'ringcentral-personal-chatbot/dist/server/models/ringcentral.js'
+import Faq from 'ringcentral-personal-chatbot-skill-faq/dist/server/model.js'
 
-const { RINGCENTRAL_CHATBOT_SERVER, SERVER_HOME = '/' } = process.env
+const {
+  RINGCENTRAL_CHATBOT_SERVER,
+  SERVER_HOME = '/',
+  RINGCENTRAL_CHATBOT_ADMIN_USERNAME, RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
+} = process.env
 const appHome = RINGCENTRAL_CHATBOT_SERVER + SERVER_HOME
 
 export const name = 'FAQ Bot'
@@ -37,3 +44,44 @@ export const onPostAdd = async ({
   }
 }
 */
+
+const auth = basicAuth({
+  users: {
+    [RINGCENTRAL_CHATBOT_ADMIN_USERNAME]: RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
+  }
+})
+
+async function viewUsers (req, res) {
+  let users = await User.findAll()
+  let r = []
+  for (let user of users) {
+    if (!user.email) {
+      let info = await user.rc.get('/restapi/v1.0/account/~/extension/~')
+      if (info && info.data) {
+        let up = {
+          email: info.data.contact.email,
+          name: info.data.name
+        }
+        await User.update(up, {
+          where: {
+            id: user.id
+          }
+        })
+        Object.assign(user, up)
+      }
+    }
+    let faqs = await Faq.findAll({
+      where: {
+        user_id: user.id
+      }
+    })
+    user = user.toJSON()
+    user.faqs = faqs.map(d => d.toJSON())
+    r.push(user)
+  }
+  res.send(r)
+}
+
+exports.appExtend = (app) => {
+  app.get('/admin/view-users', auth, viewUsers)
+}
