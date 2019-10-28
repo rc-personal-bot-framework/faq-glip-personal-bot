0ab9b655e97a1ec70d83f2018cec1a3e8df7848a
ZHAO Xudong <zxdong@gmail.com>
2019-10-28 09:58:32 +0800

    Update deps

--- src/server/lambda.js
+++ src/server/lambda.js
@@ -4,5 +4,19 @@
  */
 import serverlessHTTP from 'serverless-http'
 import app1 from './server'
+import axios from 'axios'
 
 export const app = serverlessHTTP(app1)
+
+export const maintain = async () => {
+  axios.put(
+    `${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/renew-token`,
+    undefined,
+    {
+      auth: {
+        username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
+        password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
+      }
+    }
+  )
+}
